ed898e2de0991fb1f10a99b4db6af37b617c304e
mistymidnights <mistydeveloping@gmail.com>
2022-07-23 19:26:39 +0200

    Iteración #7: Reduce

--- es6.js
+++ es6.js
@@ -258,3 +258,32 @@ console.log(findAlien)
 console.log(findMutations)
 const objUnido = { ...findAlien, mutation: findMutations };
 console.log(objUnido)
+
+/* Iteración #7: Reduce */
+//7.1 Dado el siguiente array, haz una suma de todos las notas de los examenes de 
+//los alumnos usando la función .reduce().
+
+
+const exams = [
+    {name: 'Yuyu Cabeza Crack', score: 5}, 
+    {name: 'Maria Aranda Jimenez', score: 1}, 
+    {name: 'Cristóbal Martínez Lorenzo', score: 6}, 
+    {name: 'Mercedez Regrera Brito', score: 7},
+    {name: 'Pamela Anderson', score: 3},
+    {name: 'Enrique Perez Lijó', score: 6},
+    {name: 'Pedro Benitez Pacheco', score: 8},
+    {name: 'Ayumi Hamasaki', score: 4},
+    {name: 'Robert Kiyosaki', score: 2},
+    {name: 'Keanu Reeves', score: 10}
+];
+
+const totalNotes = exams.reduce((acc, notes) => acc + notes.score, 0);
+console.log(totalNotes)
+
+//7.2 Dado el mismo array, haz una suma de todos las notas de los examenes de los 
+//alumnos que esten aprobados usando la función .reduce().
+
+const totalApproved = exams.reduce((acc, notes) => notes.score > 5 ? "" : "" );
+console.log(totalNotes)
+
+//7.3 Dado el mismo array, haz la media de las notas de todos los examenes .reduce().
